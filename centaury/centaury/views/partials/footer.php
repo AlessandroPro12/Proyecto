05b0f60fb022e9e47d1d995e3f29c3431e22f156
ALEXANDER OLIVER ROJAS <ehyker54@gmail.com>
2025-11-25 17:20:24 -0500

    Delete centaury directory

--- centaury/centaury/views/partials/footer.php
+++
@@ -1,6 +0,0 @@
-</div> <!-- cierre del flex principal -->
-<script src="../assets/js/bootstrap.bundle.min.js"></script>
-<script src="../assets/js/chart.min.js"></script>
-<script src="../assets/js/scripts.js"></script>
-</body>
-</html>
